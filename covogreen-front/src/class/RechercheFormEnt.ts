15b2d2ef77142b2bbca52dbfc8eb4368f42a2158
Mohamed El Karmoudi <karmoudiMohamed@gmail.com>
2018-02-03 12:19:04 +0100

    Ajout attributs aux entités RechercheFormEnt et TrajetEnt.

--- covogreen-front/src/class/RechercheFormEnt.ts
+++ covogreen-front/src/class/RechercheFormEnt.ts
@@ -1,2 +1,11 @@
 export class RechercheFormEnt {
+    public depart: string;
+    public destination: string;
+    public dateDepart?: date; // peut être null
+
+    constructor(depart: string, destination: string, dateDepart?: Date) {
+        this.depart = depart;
+        this.destination = destination;
+        this.dateDepart = dateDepart;
+    }
 }
--- covogreen-front/src/class/TrajetEnt.ts
+++ covogreen-front/src/class/TrajetEnt.ts
@@ -1,2 +1,17 @@
 export class TrajetEnt {
+    public id: number;
+    public depart: string;
+    public destination: string;
+    public dateDepart: Date;
+    public auteur: string;
+    public nombrePlaceDisponible: number;
+
+    constructor(id: number, depart: string, destination: string, dateDepart: Date, auteur: string, nombrePlaceDisponible: number ) {
+        this.id = id;
+        this.depart = depart;
+        this.destination = destination;
+        this.dateDepart = dateDepart;
+        this.auteur = auteur;
+        this.nombrePlaceDisponible = nombrePlaceDisponible;
+    }
 }
