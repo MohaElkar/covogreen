9b1cd2f9186d57d172667e38098d50ccb1adeb19
Mohamed El Karmoudi <karmoudiMohamed@gmail.com>
2018-02-03 18:02:41 +0100

    Ajout du formulaire (structure html)

--- covogreen-front/src/app/recherche-form/recherche-form.component.ts
+++ covogreen-front/src/app/recherche-form/recherche-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {RechercheFormEnt} from '../../class/RechercheFormEnt';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recherche-form',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recherche-form.component.css']
 })
 export class RechercheFormComponent implements OnInit {
+  rechercheFormEnt: RechercheFormEnt;
 
-  constructor() { }
+  constructor( private router: Router) {
+    this.rechercheFormEnt = new RechercheFormEnt();
+  }
 
   ngOnInit() {
   }
 
+   onSubmit() {
+       // On effectue une redirection vers la page /recherche en lui passant en parametre les valeurs renseigné dans la form.
+       this.router.navigateByUrl('/recherche', {
+           queryParams: {
+                depart: this.rechercheFormEnt.depart,
+                destination: this.rechercheFormEnt.destination,
+                date: this.rechercheFormEnt.dateDepart
+           }
+       });
+   }
+
+  // A supprimer.
+  /*get diagnostic() {
+    return JSON.stringify(this.rechercheFormEnt);
+  }*/
 }
