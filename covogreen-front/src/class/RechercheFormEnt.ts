13571d47c9af865d638b5efcd40581ee358f7a95
Mohamed El Karmoudi <karmoudiMohamed@gmail.com>
2018-02-04 17:09:43 +0100

    Ajout du filtre placeLibre. Permet à un utilisateur de filtrer les trajets ayant encore des place disponible.

--- covogreen-front/src/class/RechercheFormEnt.ts
+++ covogreen-front/src/class/RechercheFormEnt.ts
@@ -2,10 +2,12 @@ export class RechercheFormEnt {
     public depart: string;
     public destination: string;
     public dateDepart?: Date; // peut être null
+    public placeLibre: Boolean;
 
-    constructor(depart?: string, destination?: string, dateDepart?: Date) {
+    constructor(depart?: string, destination?: string, placeLibre?: Boolean, dateDepart?: Date) {
         this.depart = depart;
         this.destination = destination;
         this.dateDepart = dateDepart;
+        this.placeLibre = false;
     }
 }
