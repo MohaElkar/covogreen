c82580a8a792d5f3d3688ac9c0404e697d49b04e
Mohamed El Karmoudi <karmoudiMohamed@gmail.com>
2018-02-04 15:03:51 +0100

    Ajout de ? aux attribits du constructeur pour indiquer que l'on peut initialiser une entité sans parametre.

--- covogreen-front/src/class/RechercheFormEnt.ts
+++ covogreen-front/src/class/RechercheFormEnt.ts
@@ -1,9 +1,9 @@
 export class RechercheFormEnt {
     public depart: string;
     public destination: string;
-    public dateDepart?: date; // peut être null
+    public dateDepart?: Date; // peut être null
 
-    constructor(depart: string, destination: string, dateDepart?: Date) {
+    constructor(depart?: string, destination?: string, dateDepart?: Date) {
         this.depart = depart;
         this.destination = destination;
         this.dateDepart = dateDepart;
