2225324e53640d0c38b14ba78369b65bfac8a505
Marvin <marvin.rameix@etu.unice.fr>
2018-02-05 11:38:30 +0100

    Just adding initDB file (cleaning indexRoute)

---
+++ covogreen-back/database/initDB.js
@@ -0,0 +1,17 @@
+var co = require('co');
+
+var Car = require('/models/car');
+var User = require('/models/user');
+var Journey = require('/models/journey');
+var Participation = require('/models/participation');
+
+var initDB = co(
+    function * (){
+        yield Car.sync({force: false});
+        yield User.sync({force: false});
+        yield Journey.sync({force: false});
+        yield Participation.sync({force: false});
+    }
+);
+
+module.exports = initDB;
